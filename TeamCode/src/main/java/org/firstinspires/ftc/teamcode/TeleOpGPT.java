be6e2b224f14a29d2f8996e4d9ce1f17d7231b63
Nedtham Mekkriengkrai <nedthammek@gmail.com>
2025-10-09 11:30:47 +0700

    Add ShootingTest + TFOD Delete
    TFLOD deletion due to unusable vulforia resource

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpGPT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpGPT.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="TeleOpMode_Lite", group="TeleOp")
+@TeleOp(name="LinearOp-Team", group="TeleOp")
 public class TeleOpGPT extends LinearOpMode {
 
     private DcMotor M_LF; // มอเตอร์ซ้ายหน้า
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp (name="TeleOpMode", group="TeleOp")
+@TeleOp (name="TeleOpMode-Old", group="TeleOp")
+@Disabled
 public class TeleOpMode extends LinearOpMode {
     private DcMotor M_LR;
     private DcMotor M_RR;
